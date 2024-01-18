a120bcdf5925405d8300499a69482fad9d02f18e
Namadev Patil <nam.dpatil@gmail.com>
2024-01-18 12:22:14 +0530

    admin section completed

---
+++ src/main/java/com/shop/TestController.java
@@ -0,0 +1,16 @@
+package com.shop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class TestController {
+//	
+//	@GetMapping("/admin/categories/add")
+//	public String getCatAdd()
+//	{
+//		System.out.println("outside intered");
+//		return "categoriesAdd";
+//	}
+//
+//}
